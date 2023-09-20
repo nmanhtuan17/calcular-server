2ceca3c2ec294e3d53f53b26eb06f464a9ac722b
unknown <manhtuan17403@gmail.com>
2023-09-20 21:19:53 +0700

    add cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv')
 const db = require('./config/db/index')
 const initRoute = require('./routes/initRoute')
+const cors = require('cors')
 dotenv.config()
 
 const port = process.env.PORT || 8080
 
 const app = express()
+app.use(cors())
 app.use(express.urlencoded({
     extended: true
 }))
